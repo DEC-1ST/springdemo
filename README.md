bd8b7675eb4d15ba9498eb5627731b36275604c1
unknown <876923587@qq.com>
2019-11-22 21:34:52 +0800

    2019.11.22

--- README.md
+++ README.md
@@ -6,7 +6,15 @@
 [es社区](https://elasticsearch.cn/explore)
 [Bootstrap组件](https://v3.bootcss.com/components/)
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+[Thymeleaf](https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html)
 
 ## 工具
-[git](https://git-scm.com/)
+[Git](https://git-scm.com/)
 [VisualParadigm](https://www.visual-paradigm.com)
+[Flyway](http://ju.outofmemory.cn/entry/339528)
+[Lombok](https://projectlombok.org/)
+
+
+```bash
+mvn flyway:migrate
+```
--- src/main/java/com/lt/springdemo/controller/IndexController.java
+++ src/main/java/com/lt/springdemo/controller/IndexController.java
@@ -1,16 +1,50 @@
 package com.lt.springdemo.controller;
 
 
+import com.lt.springdemo.dto.QuestionDTO;
+import com.lt.springdemo.mapper.QuestionMapper;
+import com.lt.springdemo.mapper.UserMapper;
+import com.lt.springdemo.model.Question;
+import com.lt.springdemo.model.User;
+import com.lt.springdemo.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request,Model model) {
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 
